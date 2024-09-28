91765cf2da4de563551a4253367fe31b3dcf1261
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-09-28 18:29:35 -0300

    feat: add input to search for the location by coordinates

--- client/app/screens/Home.tsx
+++ client/app/screens/Home.tsx
@@ -13,6 +13,8 @@ import * as Location from "expo-location";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { useRouter } from "expo-router";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import globalStyles from "@/styles";
 
 export default function HomeScreen() {
   const [userLocation, setUserLocation] = useState<{
@@ -38,6 +40,7 @@ export default function HomeScreen() {
   const [newDadosPluviometricos, setNewDadosPluviometricos] = useState("");
   const router = useRouter();
   const [focusedItem, setFocusedItem] = useState(null);
+  const [search, setSearch] = useState("");
 
   const handleFocus = (item: any) => {
     setFocusedItem(item);
@@ -63,6 +66,23 @@ export default function HomeScreen() {
       console.error("Erro ao obter a localização: ", error);
       setErrorMsg("Erro ao obter a localização");
       setUserLocation(defaultLocation);
+    }
+  };
+
+  // Função para atualizar a região do mapa com base nas coordenadas
+  const handleSearch = () => {
+    const coords = search.split(",");
+    if (coords.length === 2) {
+      const latitude = parseFloat(coords[0].trim());
+      const longitude = parseFloat(coords[1].trim());
+
+      if (!isNaN(latitude) && !isNaN(longitude)) {
+        setUserLocation({ lat: latitude, lng: longitude });
+      } else {
+        alert("Por favor, insira coordenadas válidas (latitude, longitude).");
+      }
+    } else {
+      alert("Formato de coordenadas inválido. Use: 'latitude, longitude'.");
     }
   };
 
@@ -137,19 +157,43 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.HomeTitleContainer}>
+        <Text style={styles.HomeTitle}>Kersys</Text>
+        <FontAwesomeIcon
+          name="bell"
+          size={20}
+          color="#FFFFFF"
+          style={styles.notificationIcon}
+        />
+      </View>
+      <View style={styles.searchInputContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar por coordenadas (latitude, longitude)"
+          value={search}
+          onChangeText={setSearch}
+        />
+        <TouchableOpacity
+          style={styles.searchInputButton}
+          onPress={handleSearch}
+        >
+          <Text style={styles.searchButtonText}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+
       {errorMsg ? (
         <Text>{errorMsg}</Text>
       ) : userLocation ? (
         <>
           <MapView
             style={styles.map}
-            initialRegion={{
-              latitude: defaultLocation.lat,
-              longitude: defaultLocation.lng,
+            region={{
+              latitude: userLocation ? userLocation.lat : defaultLocation.lat,
+              longitude: userLocation ? userLocation.lng : defaultLocation.lng,
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0421,
             }}
-            onLongPress={handleMapLongPress} // Captura o clique e segurar no mapa
+            onLongPress={handleMapLongPress}
           >
             {userLocation && (
               <Marker
@@ -279,6 +323,57 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#f2f2f2",
   },
+  notificationIcon: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    color: "#066E3A",
+  },
+  HomeTitle: {
+    fontSize: 18,
+    color: "#066E3A",
+    fontWeight: "bold",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  HomeTitleContainer: {
+    backgroundColor: "white",
+    borderBottomColor: "#066E3A",
+    borderBottomWidth: 2, // Adicione a largura da borda aqui
+    height: 80,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  searchInputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  searchInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+    marginRight: 10,
+    height: 40,
+    borderRadius: 5,
+  },
+  inputFocused: {
+    borderColor: "#066E3A",
+    borderWidth: 1.4,
+  },
+  searchInputButton: {
+    backgroundColor: "#066E3A",
+    padding: 10,
+    borderRadius: 5,
+  },
+  searchButtonText: {
+    color: "#fff", // Cor do texto dentro do botão
+    fontWeight: "bold",
+  },
   focusedItem: {
     borderBottomWidth: 2.5,
     borderBottomColor: "#66BB6A",
@@ -310,7 +405,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: "100%",
-    height: "91%",
+    height: "75.6%",
   },
   buttonModal: {
     flexDirection: "row",
